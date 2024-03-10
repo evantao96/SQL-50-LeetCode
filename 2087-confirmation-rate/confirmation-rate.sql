029426e8a8987b2785d110ab2d0b8ddd3b013501
Evan Tao <evantao96@gmail.com>
2024-03-09 22:26:56 -0500

    Time: 1540 ms (19.92%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,17 +1,4 @@
 # Write your MySQL query statement below
--- SELECT Timeouts.user_id, num_confirmed / (num_confirmed + num_timeouts) AS confirmation_rate
--- FROM (
---     SELECT user_id, COUNT(CASE WHEN action = "timeout" THEN 1 END) as num_timeouts
---     FROM Confirmations
---     GROUP BY user_id
--- ) as Timeouts
--- INNER JOIN (
---     SELECT user_id, COUNT(CASE WHEN action = "confirmed" THEN 1 END) as num_confirmed
---     FROM Confirmations
---     GROUP BY user_id
--- ) AS Confirmed
--- ON Timeouts.user_id = Confirmed.user_id;
-
 SELECT Signups.user_id, IFNULL(ROUND(AVG(CASE WHEN action = "confirmed" THEN 1 ELSE 0 END), 2), 0) AS confirmation_rate
 FROM Confirmations
 RIGHT JOIN Signups
