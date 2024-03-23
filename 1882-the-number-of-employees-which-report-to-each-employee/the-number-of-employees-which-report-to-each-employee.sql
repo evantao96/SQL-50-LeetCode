fe5536cb2c1135f067b1ad2607eb35ec6dde2066
Evan Tao <evantao96@gmail.com>
2024-03-23 16:38:33 -0400

    Time: 1112 ms (63.69%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -4,7 +4,6 @@ FROM Employees
 INNER JOIN 
 (SELECT reports_to as employee_id, COUNT(reports_to) as reports_count, ROUND(AVG(age)) as average_age
 FROM Employees
-WHERE reports_to IS NOT NULL
 GROUP BY reports_to) as Managers
 ON Employees.employee_id = Managers.employee_id
 ORDER BY employee_id;
